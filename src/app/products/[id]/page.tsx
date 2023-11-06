a0ef412f9a4c7e96e181d1b2e6764a96d2bafb99
Raihan <raihanhn20@gmail.com>
2023-11-07 00:31:41 +0600

    product add to cart

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -2,6 +2,10 @@ import { prisma } from "@/lib/db/prisma";
 import { notFound } from "next/navigation";
 import Image from "next/image";
 import PriceTag from "@/components/PriceTag";
+import { cache } from "react";
+import { Metadata } from "next";
+import AddToCartButton from "./AddToCartButton";
+import { incrementProductQuantity } from "./actions";
 
 interface ProductPageProps {
   params: {
@@ -9,11 +13,30 @@ interface ProductPageProps {
   };
 }
 
+const getProduct = cache(async (id: string) => {
+  const product = await prisma.product.findUnique({ where: { id } });
+  if (!product) notFound();
+  return product;
+});
+
+export async function generateMetadata({
+  params: { id },
+}: ProductPageProps): Promise<Metadata> {
+  const product = await getProduct(id);
+
+  return {
+    title: product.name + " - Flowmazon",
+    description: product.description,
+    openGraph: {
+      images: [{ url: product.imageUrl }],
+    },
+  };
+}
+
 export default async function ProductPage({
   params: { id },
 }: ProductPageProps) {
-  const product = await prisma.product.findUnique({ where: { id } });
-  if (!product) notFound();
+  const product = await getProduct(id);
 
   return (
     <div className="flex flex-col gap-4 lg:flex-row lg:items-center ">
@@ -29,6 +52,10 @@ export default async function ProductPage({
         <h1 className=" text-5xl font-bold  "> {product.name} </h1>
         <PriceTag price={product.price} className="mt4" />
         <p className="py-6">{product.description} </p>
+        <AddToCartButton
+          productId={product.id}
+          incrementProductQuantity={incrementProductQuantity}
+        />
       </div>
     </div>
   );
