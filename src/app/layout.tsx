5c42ee4dfda3dc9fcc25bd286a06bb31c1db5ff6
Raihan <raihanhn20@gmail.com>
2023-11-07 17:20:23 +0600

    navbar and footer added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,36 +1,37 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "./Navbar/Navbar";
+import Footer from "./Footer";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-export const metadata= {
-  metadataBase: new URL('http://localhost:3000'),
-  title: 'Flowmazon',
-  description: 'we make your wallet cry',
- 
+export const metadata = {
+  metadataBase: new URL("http://localhost:3000"),
+  title: "Flowmazon",
+  description: "we make your wallet cry",
+
   twitter: {
-    card: 'summary_large_image',
-    site: '@eMartiiin94',
-    title: 'Title webtsite',
-    description: 'this is the desciption',
-    image: 'url/image.png'
-  }
-}
+    card: "summary_large_image",
+    site: "@eMartiiin94",
+    title: "Title webtsite",
+    description: "this is the desciption",
+    image: "url/image.png",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className=' p-4 max-w-7xl m-auto min-w-[300px] ' >
-        {children}
-        </main>
-   
-        </body>
+        <Navbar />
+        <main className=" m-auto min-w-[300px] max-w-7xl p-4 ">{children}</main>
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }
