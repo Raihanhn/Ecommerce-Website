3680dbe1afa421f3866cfb29a571162af64de1eb
Raihan <raihanhn20@gmail.com>
2023-11-08 16:31:48 +0600

    cart item add delete done

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -1,62 +1,74 @@
 import FormSubmitButton from "@/components/FormSubmitButton";
 import { prisma } from "@/lib/db/prisma";
+import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
+import { authOptions } from "../api/auth/[...nextauth]/route";
 
 export const metadata = {
-    title: "Add Product - Flowmazon"
-}
+  title: "Add Product - Flowmazon",
+};
 
 async function addProduct(formData: FormData) {
-    "use server";
+  "use server";
 
-    const name = formData.get("name")?.toString();
-    const description = formData.get("description")?.toString();
-    const imageUrl = formData.get("imageUrl")?.toString();
-    const price = Number(formData.get("price") || 0);
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    redirect("/api/auth/signin?callbckUrl=/add-product");
+  }
 
-    if(!name || !description || !imageUrl || !price){
-        throw Error("Missing required fields");
-    }
+  const name = formData.get("name")?.toString();
+  const description = formData.get("description")?.toString();
+  const imageUrl = formData.get("imageUrl")?.toString();
+  const price = Number(formData.get("price") || 0);
 
-    await prisma.product.create({
-        data: {name, description, imageUrl, price},
-    });
-    redirect("/");
+  if (!name || !description || !imageUrl || !price) {
+    throw Error("Missing required fields");
+  }
+
+  await prisma.product.create({
+    data: { name, description, imageUrl, price },
+  });
+  redirect("/");
 }
 
-export default function AddProductPage(){
-    return(
-        <div className="">
-         <h1 className="text-lg mb-3 font-bold">Add Product</h1>
-         <form action={addProduct} >
-            <input
-            required
-            name="name"
-            placeholder="Name"
-            className=" mb-3 w-full input input-bordered "
-            />
-            <textarea
-            required
-            name="description"
-            placeholder="Description"
-            className=" textarea textarea-bordered mb-3 w-full "
-            />
-             <input
-            required
-            name="imageUrl"
-            placeholder="Image URL"
-            type="url"
-            className=" mb-3 w-full input input-bordered "
-            />
-             <input
-            required
-            name="price"
-            placeholder="Price"
-            type="number"
-            className=" mb-3 w-full input input-bordered "
-            />
-            <FormSubmitButton className="btn-block" >Add Product</FormSubmitButton>
-         </form>
-        </div>
-    )
+export default async function AddProductPage() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("/api/auth/signin?callbckUrl=/add-product");
+  }
+  return (
+    <div className="">
+      <h1 className="mb-3 text-lg font-bold">Add Product</h1>
+      <form action={addProduct}>
+        <input
+          required
+          name="name"
+          placeholder="Name"
+          className=" input-bordered input mb-3 w-full "
+        />
+        <textarea
+          required
+          name="description"
+          placeholder="Description"
+          className=" textarea-bordered textarea mb-3 w-full "
+        />
+        <input
+          required
+          name="imageUrl"
+          placeholder="Image URL"
+          type="url"
+          className=" input-bordered input mb-3 w-full "
+        />
+        <input
+          required
+          name="price"
+          placeholder="Price"
+          type="number"
+          className=" input-bordered input mb-3 w-full "
+        />
+        <FormSubmitButton className="btn-block">Add Product</FormSubmitButton>
+      </form>
+    </div>
+  );
 }
